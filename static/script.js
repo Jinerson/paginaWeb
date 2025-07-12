83cf987fbcf18aeaa3ef7aa86712aab05f9c5699
Jinerson <jinersonj.perezg@utadeo.edu.co>
2025-07-12 23:28:05 +0000

    okoki

---
+++ static/script.js
@@ -0,0 +1,3 @@
+const link_registro = document.querySelector('.link-registro.registrar');
+const link_login = document.querySelector('.link-registro.login');
+const wrapper = document.querySelector('.wrapper')
