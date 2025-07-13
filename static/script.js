9f713a33056af05c4f8d59482cd1306903ec1a39
Jinerson <jinersonj.perezg@utadeo.edu.co>
2025-07-13 01:25:28 +0000

    finalizado pagina login

--- static/script.js
+++ static/script.js
@@ -1,6 +1,9 @@
 const link_registro = document.querySelector('.link-registro.registrar');
 const link_login = document.querySelector('.link-registro.ingresar');
 const wrapper = document.querySelector('.wrapper');
+const btn_popup = document.querySelector('.btonEntrarRegistro');
+const iconClose = document.querySelector('.icon-close');
+
 
 link_registro.addEventListener('click', () => {
     wrapper.classList.add('active');
@@ -10,3 +13,10 @@ link_login.addEventListener('click', () => {
     wrapper.classList.remove('active');
 });
 
+btn_popup.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
+});
